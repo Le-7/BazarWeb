bb7777ca732c1196d8528c3f9a26de191740a40b
RolexSir30 <saaruhan75@gmail.com>
2023-12-01 18:48:49 +0100

    pageAdminConnected html

--- src/main/java/controller/AdministrateurController.java
+++ src/main/java/controller/AdministrateurController.java
@@ -1,13 +1,18 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import model.Customer;
 import service.AdministrateurService;
+import service.CustomerService;
 
 @Controller
 @RequestMapping("/administrateur")
@@ -15,7 +20,8 @@ public class AdministrateurController {
 
 	@Autowired
 	private AdministrateurService administrateurService;
-	
+	private CustomerService customerservice ;
+
 	
 	public AdministrateurController(AdministrateurService administrateurService) {
 		super();
@@ -27,12 +33,26 @@ public class AdministrateurController {
 		return "connectionAdmin";
 	}
 	
+	
+	@PostMapping("/infoClient")
+	public String pageConnectionInfoClient(@RequestParam String username) {
+		return "infoClient";
+	}
+	
 	@PostMapping("/connected")
-	public String ConnectionCustomer(@RequestParam String username, @RequestParam String password) {
+	public String ConnectionCustomer(@RequestParam String username, @RequestParam String password,Model model) {
 		if(administrateurService.connectionAdministrateur(username,password)) {
-			return "redirect:/product/product";
+			  // Assuming you have a service class that retrieves the list of clients from the database
+	        List<Customer> clients = CustomerService.getAllClients();
+
+	        // Add the list of clients to the model
+	        model.addAttribute("clients", clients);
+
+	        //return "yourPage";
+			
+			return "adminConnected";
 		}else{
-			return "redirect:/#";
+			return "connectionAdmin";
 		}
 	}
 }
--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,11 @@ public class CustomerService {
         // If a customer is found, return true; otherwise, return false
         return optionalCustomer.isPresent();
     }
+
+	public static List<Customer> getAllClients() {
+		return customerRepository.findAll();
+		// TODO Auto-generated method stub
+	}
 
     
     
